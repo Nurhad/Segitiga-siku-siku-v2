876a2677be486d03c04fbc2d3b3e2c20299df4c0
Muhammad Barokah <m.nurhadbarokah@gmail.com>
2020-01-14 14:38:42 +0700

    Add files via upload

---
+++ segitigaVersi2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace segitigaVersi2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
